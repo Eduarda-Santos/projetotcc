3a922cb9d4f80633ac81bd4a330e75a74e0d74a0
Eduarda-Santos <iameduardas@gmail.com>
2022-11-11 11:27:31 -0300

    Model Vacina

---
+++ app/Models/Vacina.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Vacina extends Model
+{
+    protected $fillable = ['nome','observacao'];
+    use HasFactory;
+}
