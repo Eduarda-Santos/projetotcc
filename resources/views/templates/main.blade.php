79b3ca9842e60a3d5016799f3f655e3e38b70995
Eduarda-Santos <iameduardas@gmail.com>
2022-11-11 11:28:07 -0300

    Create Templates

--- resources/views/templates/main.blade.php
+++ resources/views/templates/main.blade.php
@@ -6,7 +6,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <!-- <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous"> -->
     <link href="{{ asset('css/bootstrap.min.css') }}" rel="stylesheet">
-    <title>SysNAPNE - Sistema Integrado de Dados</title>
+    <title>MCI</title>
 
     <style> 
         .input-group-text { 
@@ -18,12 +18,8 @@
     <nav class="navbar sticky-top navbar-expand-md navbar-dark bg-success">
         <div class="container-fluid">
         <a href="{{route('index')}}" class="navbar-brand ms-sm-3">
-                <svg xmlns="http://www.w3.org/2000/svg" width="36" height="36" fill="currentColor" class="bi bi-server" viewBox="0 0 16 16">
-                    <path d="M1.333 2.667C1.333 1.194 4.318 0 8 0s6.667 1.194 6.667 2.667V4c0 1.473-2.985 2.667-6.667 2.667S1.333 5.473 1.333 4V2.667z"/>
-                    <path d="M1.333 6.334v3C1.333 10.805 4.318 12 8 12s6.667-1.194 6.667-2.667V6.334a6.51 6.51 0 0 1-1.458.79C11.81 7.684 9.967 8 8 8c-1.966 0-3.809-.317-5.208-.876a6.508 6.508 0 0 1-1.458-.79z"/>
-                    <path d="M14.667 11.668a6.51 6.51 0 0 1-1.458.789c-1.4.56-3.242.876-5.21.876-1.966 0-3.809-.316-5.208-.876a6.51 6.51 0 0 1-1.458-.79v1.666C1.333 14.806 4.318 16 8 16s6.667-1.194 6.667-2.667v-1.665z"/>
-                  </svg>
-                <span class="ms-3 fs-5">SysNAPNE</span>
+                <img src="{{ asset('images/map.jpg') }}" alt="map">
+                <span class="ms-3 fs-5">MCI - MONITORAMENTO DE CONTAMINAÇÕES EM INSTITUIÇÕES</span>
             </a>
             <div class="collapse navbar-collapse" id="itens">
                 <ul class="navbar-nav ms-auto">
