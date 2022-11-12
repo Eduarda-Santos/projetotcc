462ccd85d1982aadf6eda22740717fc6430263f0
Eduarda-Santos <iameduardas@gmail.com>
2022-11-11 23:36:23 -0300

    Migration Contaminação

---
+++ database/migrations/2022_11_12_011439_contaminacao.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('contaminacoes', function (Blueprint $table) {
+            $table->id();
+            $table->date('dataInicioSintomas');
+            $table->date('dataInicioAfastamento');
+            $table->date('dataRealizacaoExame');
+            $table->string('resultadoExame');
+            $table->date('dataTerminoAfastamento');
+            $table->text('anexo');
+            $table->string('descricao');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('contaminacoes');
+    }
+};
