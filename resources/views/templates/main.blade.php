78c0877603d8165019cc338366cd63f1302026c5
Eduarda-Santos <iameduardas@gmail.com>
2022-11-11 23:39:48 -0300

    Update Main

--- resources/views/templates/main.blade.php
+++ resources/views/templates/main.blade.php
@@ -29,10 +29,12 @@
                                 <path fill-rule="evenodd" d="m8 3.293 6 6V13.5a1.5 1.5 0 0 1-1.5 1.5h-9A1.5 1.5 0 0 1 2 13.5V9.293l6-6zm5-.793V6l-2-2V2.5a.5.5 0 0 1 .5-.5h1a.5.5 0 0 1 .5.5z"/>
                                 <path fill-rule="evenodd" d="M7.293 1.5a1 1 0 0 1 1.414 0l6.647 6.646a.5.5 0 0 1-.708.708L8 2.207 1.354 8.854a.5.5 0 1 1-.708-.708L7.293 1.5z"/>
                               </svg>
-                            <span class="ps-1 text-white">IFPR</span>
+                            <span class="ps-1 text-white">Opções</span>
                         </a>
                         <ul class="dropdown-menu">
-            
+                            <li><a href="{{route('funcionarios.index')}}" class="dropdown-item">Funcionarios</a></li>
+                            <li><a href="{{route('vacinas.index')}}" class="dropdown-item">Vacinas</a></li>
+                            <li><a href="{{route('areas.index')}}" class="dropdown-item">Areas</a></li>
                         </ul>
                     </li>
                     <li class="nav-item ps-2 me-3">
