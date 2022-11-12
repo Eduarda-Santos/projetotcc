78354b723a2ce2f99067350b2c10eeb5626149f7
Eduarda-Santos <iameduardas@gmail.com>
2022-11-11 23:39:24 -0300

    Create Views de VacinasFuncionarios

---
+++ resources/views/vacinasFuncionario/index.blade.php
@@ -0,0 +1,22 @@
+@extends('templates.main', ['titulo' => "Vacinas dos Funcionários", 'rota' => "vacinasFuncionario.create"])
+
+@section('titulo') Vacinas dos Funcionários @endsection
+@section('conteudo')
+
+    <div class="row">
+        <div class="col">
+            
+            <x-datalist
+                :title="'Vacinas dos Funcionários'"
+                :header="['DOSE','DATA DA VACINA','LOTE']" 
+                :data="$data"
+                :fields="['dose','data da vacina','lote']"
+                :hide="[true, true, false]" 
+                :crud="'vacinasFuncionario'"
+                :info="['dose','data da vacina','lote']"
+                :remove="'nome'"
+            />
+
+        </div>
+    </div>
+@endsection
