d70bd8b9d494eb6b4441c2e080bb263f9542e870
Eduarda-Santos <iameduardas@gmail.com>
2022-11-11 23:37:24 -0300

    Update View Areas

---
+++ resources/views/areas/index.blade.php
@@ -0,0 +1,22 @@
+@extends('templates.main', ['titulo' => "Áreas", 'rota' => "areas.create"])
+
+@section('titulo') Cursos @endsection
+@section('conteudo')
+
+    <div class="row">
+        <div class="col">
+            
+            <x-datalist
+                :title="'Áreas'"
+                :header="['NOME','AÇÕES']" 
+                :data="$data"
+                :fields="['nome']"
+                :hide="[true, false]" 
+                :crud="'areas'"
+                :info="['nome']"
+                :remove="'nome'"
+            />
+
+        </div>
+    </div>
+@endsection
