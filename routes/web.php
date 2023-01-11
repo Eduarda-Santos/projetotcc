cb9a0c2870d27de82428c854178b84e7aeeefd61
Eduarda-Santos <iameduardas@gmail.com>
2023-01-11 17:34:32 -0300

    Create Auth

--- routes/web.php
+++ routes/web.php
@@ -1,9 +1,8 @@
 <?php
 
+use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
-use App\Models\Vacina;
-use App\Models\VacinaFuncionario;
-use App\Http\Middleware\Mid;
+
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -14,71 +13,19 @@ use App\Http\Middleware\Mid;
 | contains the "web" middleware group. Now create something great!
 |
 */
-Route::get('/', function() {
-    return view('welcome');
-})->middleware('Mid');
 
 Route::get('/', function () {
-    return view('templates.main')->with('titulo', "");
-})->name('index');
-
-Route::redirect('/funcionarios', 301);
-Route::resource('funcionarios', 'FuncionarioController');
-
-Route::redirect('/areas', 302);
-Route::resource('areas', 'AreaController');
-
-Route::redirect('/exames', 304);
-Route::resource('exames', 'ExameController');
-
-Route::redirect('/vacinas', 303);
-Route::resource('vacinas', 'VacinaController');
-
-Route::redirect('/vacinasFuncionario', 305);
-Route::resource('vacinasFuncionario', 'VacinaFuncionarioController');
-
-Route::redirect('/contaminacoes', 306);
-Route::resource('contaminacoes', 'ContaminacaoController');
-
-Route::post('vacinas/add', function(Request $request) {
-    
-    $obj_vacinafuncionario = new VacinaFuncionario();
-    $obj_vacinafuncionario->nome = mb_strtoupper($request->nome_vacinafuncionario, 'UTF-8');
-    $obj_vacinafuncionario->observacao = mb_strtoupper($request->obs_vacinafuncionario, 'UTF-8');
-    $obj_vacinafuncionario->save();
-
-    $table->unsignedBigInteger('vacina_id');
-    $table->foreign('vacina_id')->references('id')->on('vacinas');
-    $table->integer('dose');
-    $table->unsignedBigInteger('funcionario_id');
-    $table>foreign('funcionario_id')->references('id')->on('funcionarios');
-    $table->date('dataVacina');
-    $table->string('lote');
-    
-    $obj_vacina = new Vacina();
-    $obj_vacina->nome = mb_strtoupper($request->nome_vacina, 'UTF-8');
-    $obj_vacina->observacao = mb_strtoupper($request->obs_vacina, 'UTF-8');
-    
-    $obj_vacinafuncionario->vacina()->associate($obj_vacina);
-    $obj_vacinafuncionario->save();
-    return "<h1>Vacina Cadastrada com Sucesso!</h1>";
+    return view('welcome');
 });
 
-Route::get('/vacinas', function () {
-    $obj_vacina = Vacina::with(['vacinafuncionario'])->get();
-    return $obj_vacina->toJson();
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
+
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
 
-Route::get('/vacinafuncionario', function () {
-    $obj_vacinafuncionario = VacinaFuncionario::with(['vacinas'])->get();
-    return $obj_vacinafuncionario->toJson();
-});
-
-Route::get('/vacinas', 'VacinaController@index')
-->middleware('Mid', 'NewMid');
-
-Route::get('/vacinas/{id}', 'VacinaController@show')
-->middleware('Mid');
-
-
-    
+require __DIR__.'/auth.php';
