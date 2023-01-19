1f1626564ac254f53ddc3cdab7655861873b0c78
Eduarda-Santos <iameduardas@gmail.com>
2023-01-18 23:35:30 -0300

    Update Policy

--- resources/views/funcionarios/index.blade.php
+++ resources/views/funcionarios/index.blade.php
@@ -1,7 +1,18 @@
 @extends('templates.middleware', ['titulo' => "Funcionários", 'rota' => "funcionarios.create"])
 
-@section('titulo') Cursos @endsection
+@section('titulo') Funcionários @endsection
 @section('conteudo')
+
+@can('update', $item)
+<a href= "{{ route('funcionarios.edit', $item) }}" class="btn btn-success"></a>
+@endcan
+@can('view', $item)
+<a href= "{{ route('funcionarios.show', $item) }}" class="btn btn-primary"></a>
+@endcan
+@can('delete', $item)
+<a nohref style="cursor:pointer" onclick="document.getElementById('form_1').submit()" class="btn btn-danger"></a>
+@endcan
+
 
     <div class="row">
         <div class="col">
